d0de1bf1195a187946a3d612ecba14f5ba8108d0
김다영 <darong9494@gmail.com>
2026-04-23 17:26:32 +0900

    Board

--- src/main/java/com/tenco/blog/controller/UserController.java
+++ src/main/java/com/tenco/blog/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.tenco.blog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+    // 회원가입 페이지 요청
+    // 주소설계 : http://localhost:8080/join-form
+    @GetMapping("/join-form")
+    public String joinForm(){
+        // 뷰 리절브 동작
+        // classPath: src/main/resources/templates/ 기본이 여기까지임
+        return "user/join-form";
+    }
 }
